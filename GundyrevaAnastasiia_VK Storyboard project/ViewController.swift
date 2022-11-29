f3ec6745d6c76452af65e98ef13bed434718ceaa
Asya Checkanar <asyach3ck@gmail.com>
2022-11-29 20:16:40 +0300

    Initial Commit

---
+++ GundyrevaAnastasiia_VK Storyboard project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GundyrevaAnastasiia_VK Storyboard project
+//
+//  Created by Asya Checkanar on 29.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
