3cb6526459e5d2491f7bf5ef46421f23ca119fb7
Asya Checkanar <asyach3ck@gmail.com>
2023-03-30 23:36:38 +0300

    added button, still has bugs

--- GundyrevaAnastasiia_VK Storyboard project/Newsfeed Service/PostTextCell.swift
+++ GundyrevaAnastasiia_VK Storyboard project/Newsfeed Service/PostTextCell.swift
@@ -10,6 +10,29 @@ import UIKit
 class PostTextCell: UITableViewCell {
 
     @IBOutlet weak var postText: UILabel!
+//    private var showButton: ShowMoreButton!
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        postText.addSubview(showButton)
+//        contentView.addSubview(showButton)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+       super.init(coder: aDecoder)
+    }
+    
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        let buttonWidth: CGFloat = 100
+//        let buttonHeight = postText.frame.size.height/4
+//        showButton.frame = CGRect(x: postText.frame.size.width - buttonWidth,
+//                                  y: postText.frame.size.height - buttonHeight,
+//                                  width: buttonWidth,
+//                                  height: buttonHeight)
+//
+//
+//    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
